388017f4224fae5acd5476138c271296e9c529ed
season1618 <seasonminority314@gmail.com>
2023-03-29 18:16:27 +0900

    zoom by mouse wheel.

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,10 +1,28 @@
 import './Canvas.css';
 import { Expr } from '../converter/parser';
-import { useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { TreeContext } from '../App';
 
 function Canvas() {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [origin, setOrigin] = useState({ x: 0, y: 0 });
+  const [logScale, setLogScale] = useState(0);
   const [tree, setTree] = useContext(TreeContext);
+
+  function updateMousePos(x: number, y: number) {
+    setMousePos({ x, y });
+  }
+
+  function updateScale(delta: number) {
+    let nextX = mousePos.x + Math.pow(1.1, delta/100) * (origin.x - mousePos.x);
+    let nextY = mousePos.y + Math.pow(1.1, delta/100) * (origin.y - mousePos.y);
+    console.log(mousePos, nextX, nextY, origin.x, origin.y);
+    let nextLogScale = logScale + delta / 100;
+    if (-50 < nextLogScale && nextLogScale < 50) {
+      setOrigin({ x: nextX, y: nextY });
+      setLogScale(nextLogScale);
+    }
+  }
 
   useEffect(
     () => {
@@ -24,7 +42,12 @@ function Canvas() {
     () => {
       const canvas = document.querySelector('canvas') as HTMLCanvasElement;
       const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+      
+      context.resetTransform();
       context.clearRect(0, 0, canvas.width, canvas.height);
+      context.translate(origin.x, origin.y);
+      context.scale(Math.pow(1.1, logScale), Math.pow(1.1, logScale));
+      
 
       const width = 80;
       const height = 120;
@@ -47,7 +70,7 @@ function Canvas() {
             let num = expr.args.length;
             expr.args.forEach((child, index) => {
               draw(child);
-              
+
               let x1 = width * expr.offset + (index + 1) / (num + 1) * 2 * r - r;
               let y1 = height * expr.depth + r;
               let x2 = width * child.offset;
@@ -68,11 +91,19 @@ function Canvas() {
         }
       }
     },
-    [tree]
-  )
+    [origin, logScale, tree]
+  );
 
   return (
-    <canvas></canvas>
+    <canvas
+      onMouseMove={
+        (e) => {
+          let target = e.currentTarget.getBoundingClientRect();
+          updateMousePos(e.clientX - target.left, e.clientY - target.top);
+        }
+      }
+      onWheel={(e) => updateScale(-e.deltaY)}
+    ></canvas>
   )
 }
 
