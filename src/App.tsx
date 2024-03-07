588855844fc2caa3a273a00ac653a64765b91c56
season1618 <seasonminority314@gmail.com>
2024-03-07 16:40:20 +0900

    initialize split bar position at the half of window

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,11 @@ import { Node } from './converter/layout';
 const initNode: Node | undefined = undefined;
 export const NodeContext = createContext<[Node | undefined, (node: Node | undefined) => void]>([initNode, (node: Node | undefined) => {}])
 export const DragContext = createContext<[boolean, (dragged: boolean) => void]>([false, (dragged: boolean) => {}])
-export const BorderContext = createContext<[number, (borderX: number) => void]>([200, (borderX: number) => {}])
+export const BorderContext = createContext<[number, (borderX: number) => void]>([0, (borderX: number) => {}])
 
 function App() {
   const [dragged, setDragged] = useState<boolean>(false);
-  const [borderX, setBorderX] = useState<number>(100);
+  const [borderX, setBorderX] = useState<number>(window.innerWidth / 2);
   const [node, setNode] = useState<Node | undefined>(initNode);
 
   return (
